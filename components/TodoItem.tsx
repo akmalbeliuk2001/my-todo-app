a618e7dfece2a82ac09a4658402be90d04e248ed
Akmal <akmalbeliuk@gmail.com>
2025-07-16 00:29:36 +0700

    simple todo

---
+++ components/TodoItem.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useTodoStore } from "@/store/todoStore";
+
+export default function TodoItem() {
+  const { todos, toggleTodo, deleteTodo } = useTodoStore();
+
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id} className="flex gap-2">
+          <input
+            type="checkbox"
+            checked={todo.completed}
+            onChange={() => toggleTodo(todo.id)}
+          />
+          <span className={todo.completed ? "line-through" : ""}>
+            {todo.text}
+          </span>
+          <button onClick={() => deleteTodo(todo.id)}>🗑</button>
+        </li>
+      ))}
+    </ul>
+  );
+}
